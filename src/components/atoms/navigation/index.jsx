777ff8a2b8966852ede963d47f679dea46bad748
m.samimi <samimi.dev@gmail.com>
2022-01-29 14:19:33 +0330

    cleane code

--- src/components/atoms/navigation/index.jsx
+++ src/components/atoms/navigation/index.jsx
@@ -1,33 +1,31 @@
 import { NavLink } from "react-router-dom";
 import useStyles from "./style";
-import { View ,Text} from "reactjs-view";
-import { useCart } from "../../../core/constans/provider/cartProvider";
+import { View, Text } from "reactjs-view";
 import { Routes } from "../../../core/constans/routes";
-import { Badge } from "antd";
 
 const Navigation = () => {
-	const { cart } = useCart();
-	const classes = useStyles();
-	return (
-		<View className={classes.mainHeader}>
-			<NavLink
-				className={(navData) =>
-					navData.isActive ? classes.activeLink : classes.navlink
-				}
-				to={Routes.homepage.create()}>
-          <Text size="large">Home</Text>
-				
-			</NavLink>
-			<View style={{ paddingInline: 16 }}>
-				<NavLink
-					className={(navData) =>
-						navData.isActive ? classes.activeLink : classes.navlink
-					}
-					to={Routes.cart.create()}>
+  const classes = useStyles();
+  return (
+    <View className={classes.mainHeader}>
+      <NavLink
+        className={(navData) =>
+          navData.isActive ? classes.activeLink : classes.navlink
+        }
+        to={Routes.homepage.create()}
+      >
+        <Text size="large">Home</Text>
+      </NavLink>
+      <View style={{ paddingInline: 16 }}>
+        <NavLink
+          className={(navData) =>
+            navData.isActive ? classes.activeLink : classes.navlink
+          }
+          to={Routes.cart.create()}
+        >
           <Text size="large">Cart</Text>
-				</NavLink>
-			</View>
-		</View>
-	);
+        </NavLink>
+      </View>
+    </View>
+  );
 };
 export { Navigation };
