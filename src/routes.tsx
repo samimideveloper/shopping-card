ac4df5a46c81d6a6150fb25d4496b4ee2941941b
m.samimi <samimi.dev@gmail.com>
2022-02-23 09:48:17 +0330

    clean code

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,19 +3,17 @@ import { Navigate, Route, Routes as Switche } from "react-router-dom";
 import { HomePage } from "./components/pages/homepage";
 import { CartPage } from "./components/pages/cart";
 import { View } from "reactjs-view";
-import { Navigation } from "./components/atoms/navigation";
 
 function Router() {
-	return (
-		<View>
-			{/* <Navigation /> */}
-			<Switche>
-				<Route path="*" element={<Navigate to={Routes.homepage.create()}/>} />
-				<Route path={Routes.homepage.template()} element={<HomePage />} />
-				<Route path={Routes.cart.template()} element={<CartPage />} />
-			</Switche>
-		</View>
-	);
+  return (
+    <View>
+      <Switche>
+        <Route path="*" element={<Navigate to={Routes.homepage.create()} />} />
+        <Route path={Routes.homepage.template()} element={<HomePage />} />
+        <Route path={Routes.cart.template()} element={<CartPage />} />
+      </Switche>
+    </View>
+  );
 }
 
 export default Router;
