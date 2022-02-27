08683c89fc816570261dff296fe9f43c0994c3e5
m.samimi <samimi.dev@gmail.com>
2022-02-27 09:40:26 +0330

    change login page background

--- src/components/pages/login/style.ts
+++ src/components/pages/login/style.ts
@@ -7,7 +7,10 @@ export default createUseStyles(
       marginInline: "auto",
       justifyContent: "center",
       alignItems: "center",
-      height: "600px",
+      height: "100vh",
+      backgroundColor: "rgb(255,255,255)",
+      background:
+        " linear-gradient(275deg, rgba(255,255,255,1) 0%, rgba(233,123,126,1) 37%, rgba(255,255,255,1) 100%)",
     },
   },
   { index: generateIndex("pages") }
