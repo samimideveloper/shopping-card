5b0b32bf19a148394b27e2d405fda995ee55969b
m.samimi <samimi.dev@gmail.com>
2022-04-09 17:19:44 +0430

    change color lable

--- src/common/input.js
+++ src/common/input.js
@@ -2,7 +2,9 @@ import { View, Text } from "reactjs-view";
 const Input = ({ label, name, formik, type = "text" }) => {
   return (
     <View>
-      <Text htmlFor={name}>{label}</Text>
+      <Text color="#000" htmlFor={name}>
+        {label}
+      </Text>
       <input
         id={name}
         type={type}
