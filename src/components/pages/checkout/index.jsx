cdd1242008b0e8b501546848a563a33c1c88f611
m.samimi <samimi.dev@gmail.com>
2022-04-24 15:13:14 +0430

    add total to cart

--- src/components/pages/checkout/index.jsx
+++ src/components/pages/checkout/index.jsx
@@ -1,9 +1,12 @@
 import { View, Text } from "reactjs-view";
+import { useCart } from "../../../core/constans/provider/cartProvider";
 
 const CheckOut = () => {
+  const total = useCart();
   return (
     <View>
       <Text theme="bold">صفحه پرداخت</Text>
+      <Text theme="bold">{total} </Text>
     </View>
   );
 };
